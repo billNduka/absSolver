9c23a31d48e2d54a35ac990db978ea9f98d9795a
billNduka <153222779+billNduka@users.noreply.github.com>
2023-12-23 16:28:22 +0100

    Add files via upload
    Made massive improvements to this including;
    - Addition of an input textbox for the binomial and power
    - Now accepts any letter as a variable
    - Upgraded the calculator to expand terms containing variables

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const disp = document.getElementById("display-el")
 const btn = document.getElementById("submit-btn")
-const appear = setInterval(() => {disp.innerHTML = "<b>_</b>"},1000)
-const disappear = setInterval(() => {disp.innerText = " "},2000)
+const input = document.getElementById("input-el")
+const power = document.getElementById("input-sq")
+btn.addEventListener("click", solve)
+let a
+let b
+let letters
+let digits
+let coefficient
+// const appear = setInterval(() => {input.placeholder = "_"},1000)
+// const disappear = setInterval(() => {input.placeholder = " "},1500)
 
-function solve(){
+// function cursor(){
+//     clearInterval(appear)
+//     clearInterval(disappear)
+// }
+
+/*function solve(){
     let a = Number(prompt("What is your a?"))
     let b = Number(prompt("What is your b?"))
     let n = Number(prompt("What is your n?"))
@@ -14,6 +27,15 @@ function solve(){
     }
     
 
+}*/
+function solve(){
+    letters = input.value.match( /[a-zA-Z]/g)
+    digits = input.value.match( /[\d]+/g)
+    coefficient = letters[0]
+    a = Number(digits[0])
+    b = Number(digits[1])
+    let n = power.value
+    expand(a,b,n)
 }
 function factorial(n){
     let result = 1
@@ -27,19 +49,62 @@ function combine(n,r){
     return(answer)
 }
 function formatting(a){
-    if(a[a.length-2] == "+ "){
         a.slice(0,-2)
-    }
 }
-function expand(a,bx,n){
-    clearInterval(appear)
-    clearInterval(disappear)
-    let expansion = ""
-    for(i=0;i<=n;i++){
-    expansion += combine(n,i)*(a**(n-i))*(bx**i) + `x<sup>${i}</sup>+ `}
+
+
+
+const add = (x, y) => {
+    while(y !== 0){
+       let carry = x & y;
+       x = x ^ y;
+       y = carry << 1;
+    }
+    return x;
+ };
+
+function expand(a,b,n){
+    if(/\^\d+/.test(input.value)){
+        if(input.value.match(/[a-zA-Z]/g).length > 1){
+            let expansion = ""
+            coefficient = letters[0]
+            let power1 = input.value.match(/\^\d+/)[0]
+            let power2 = input.value.match(/\^\d+/g)[1]
+            let inc2
+                for(i=n; i>=0;i--){
+                let increment = Number(power1[1])               
+                if(input.value.match(/\^\d+/g).length == 1){
+                    expansion += combine(n,i)*(a**(n-i))*(b**i) + `${coefficient}<sup>${1+add(increment,i)}</sup>+ `
+                } else {
+                    inc2 = Number(power2[1])
+                    expansion += combine(n,i)*(a**(n-i))*(b**i) + `${coefficient}<sup>${inc2+add(increment,i)}</sup>+ `
+                } 
+                }
+            expansion = expansion.slice(0,-2)
+            disp.innerHTML = expansion
+            console.log(expansion)
+        } else{
+            let expansion = ""
+            coefficient = letters[0]
+            let power1 = input.value.match(/\^\d+/)[0]
+                for(i=n; i>=0;i--){
+                let increment = Number(power1[1])
+                expansion += combine(n,i)*(a**(n-i))*(b**i) + `${coefficient}<sup>${add(increment,i)}</sup>+ `
+                }
+            expansion = expansion.slice(0,-2)
+            disp.innerHTML = expansion
+            console.log(expansion)
+        }
+    } else {
+        let expansion = ""
+        coefficient = letters[0]
+        for(i=n; i>=0;i--){
+        expansion += combine(n,i)*(a**(n-i))*(b**i) + `${coefficient}<sup>${i}</sup>+ `
+       }
     expansion = expansion.slice(0,-2)
-    expansion = expansion.replace("x<sup>0</sup>", " ")
-    expansion = expansion.replace(" 1x", "x")
-    expansion = expansion.replace("<sup>1</sup>", " ")
     disp.innerHTML = expansion
-}   
+    console.log(expansion)
+   
+} 
+}
+
